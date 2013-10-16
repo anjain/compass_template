d231d473f0c2cae8306cf60953b838827923c58a
anjain <ankur.jain@ongraph.com>
2013-10-16 13:18:17 +0530

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Clipeo::Application.config.secret_key_base = '9ca50678d70eb027cd6c0ee0fda11adf7c886feed77c86c84c17747fa5c7510b1656f40788ec2fdd14c2944fcf3cfc94f4a53a2be9cf47c3b3bc92f541278ad7'
